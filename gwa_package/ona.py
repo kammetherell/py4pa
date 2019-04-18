7d954e65be8650d302824506ecc1b7c0669d75d4
Kevin Dickens <kevindickens@gmail.com>
2019-04-18 16:41:43 +0100

    Add Docstrings to functions

--- gwa_package/ona.py
+++ gwa_package/ona.py
@@ -5,6 +5,32 @@ import os
 
 
 def clean_email_data(dir, files='all', include_subject=False, engine='c', encoding='latin', delete_old_file=False):
+    """Cleans email data from Splunk to key fields only
+
+    Parameters
+    ----------
+    dir: String
+        Path to the root directory containing the data files to process
+
+    file: List or 'all' (optional)
+        List containing all files to be processed within the directory. If 'all' passed, then all CSV files in the directory will be processed
+
+    include_subject: Boolean default = False
+        Defines whether the Subject field should be included in the cleaned data
+
+    engine: String default='c'
+        The Pandas engine to read in the data, either 'c' or 'python'
+
+    encoding: String default = 'latin'
+        The file encoding of files to be read
+
+    delete_old_file: Boolean default = False
+        If set to True, the original Splunk data file will be delete_old_file
+
+    Returns
+    -------
+    Nothing is returned by the function, but new files are written to 'dir' that have been cleaned
+    """
     start = datetime.datetime.now()
     print('Starting: ', str(start.hour).zfill(2) + ':'+ str(start.minute).zfill(2))
 
@@ -61,12 +87,18 @@ def calc_density(df_nodes, df_edges, target_attribute):
 
     Parameters
     ----------
-    df_nodes
+    df_nodes: Pandas DataFrame
+        Dataframe containing the Node list
 
+    df_edges: Pandas DataFrame
+        DataFrame containing the edge list
+
+    target_attribute: String
+        Name of attribute in Node List that we want to calculate the densities between
 
     Returns
     ----------
-    Pandas DataFrame containing the densities
+    Pandas DataFrame containing the densities, grouped by the target_attribute values
     """
 
     df_edges['sender_group'] = df_edges['SenderAddress'].map(df_nodes.set_index('email')[target_attribute])
@@ -107,7 +139,23 @@ def _get_modularity(df_edges, row, target_attribute, weighted, source):
 
 
 def calc_modularity(df_nodes, df_edges, target_attribute, weighted=False, source='sender'):
+    """Calculates the Modularity of connections originating from groups in a specific target target_attribute
 
+    Parameters
+    ----------
+    df_nodes: Pandas DataFrame
+        Dataframe containing the Node list
+
+    df_edges: Pandas DataFrame
+        DataFrame containing the edge list
+
+    target_attribute: String
+        Name of attribute in Node List that we want to calculate the modularities between
+
+    Returns
+    ----------
+    Pandas DataFrame containing the modularities, grouped by the target_attribute values
+    """
     if source == 'sender':
         source_group = 'sender_group'
         end_group = 'recipient_group'
