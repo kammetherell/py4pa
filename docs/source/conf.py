6d8651559b8f6ae08e2efbc948ff2db30fff0810
Kevin Metherell <kevin.metherell@experian.com>
2024-02-23 14:31:58 +0000

    update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Kevin Metherell'
 author = 'Kevin Metherell'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.1.6'
 
 
 # -- General configuration ---------------------------------------------------
